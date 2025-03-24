ae38d8e28b8c6e9c28092553fa8d9d66282275b2
Brett G <brett.g@digitalmass.com>
2025-03-24 16:18:46 -0500

    Chapter 15: Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { error } from "console";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -120,3 +122,22 @@ export async function deleteInvoice(id: string) {
 	}
 	revalidatePath("/dashboard/invoices");
 }
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData,
+  ) {
+	try {
+	  await signIn('credentials', formData);
+	} catch (error) {
+	  if (error instanceof AuthError) {
+		switch (error.type) {
+		  case 'CredentialsSignin':
+			return 'Invalid credentials.';
+		  default:
+			return 'Something went wrong.';
+		}
+	  }
+	  throw error;
+	}
+  }
